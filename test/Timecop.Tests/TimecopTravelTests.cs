2f83c4cb36b43d1bb6435ed2e865177a1eb872a1
Dmytro Khmara <dmytro@dmytrokhmara.com>
2023-07-29 13:15:23 +0100

    feat: add the TravelTo method. (#6)

--- test/Timecop.Tests/TimecopTravelTests.cs
+++ test/Timecop.Tests/TimecopTravelTests.cs
@@ -1,9 +1,13 @@
 using FluentAssertions;
+using System.Reflection.Metadata;
 
 namespace TCop.Tests
 {
     public class TimecopTravelTests
     {
+
+        private static readonly TimeSpan DateTimeComparisonPrecision = TimeSpan.FromMilliseconds(50);
+
         [Fact]
         public void TravelBy_InFrozenState_ShouldFreezeAndTravel_AndReturnCurrentUtcTime()
         {
@@ -12,6 +16,68 @@ namespace TCop.Tests
             var traveledTo = tc.TravelBy(TimeSpan.FromDays(3));
 
             traveledTo.Should().Be(frozenAt.AddDays(3));
+        }
+
+        [Fact]
+        public void TravelToDateTime_ShouldTravelToSpecifiedDateTime_AndReturnThatDateTime()
+        {
+            using var tc = new Timecop();
+
+            var travelTo = DateTime.Now.AddDays(-7);
+
+            var traveledTo = tc.TravelTo(travelTo);
+
+            Thread.Sleep(100);
+
+            Clock.Now.Should().BeCloseTo(
+                traveledTo.Add(TimeSpan.FromMilliseconds(100)), DateTimeComparisonPrecision);
+
+            traveledTo.Should().Be(travelTo);
+        }
+
+        [Fact]
+        public void TravelToLocalDateAndTime_ShouldTravelToGivenDateAndTime_AndReturnLocalTime()
+        {
+            using var tc = new Timecop();
+
+            var traveledTo = tc.TravelTo(1990, 12, 2, 14, 34, 55, DateTimeKind.Local);
+            var expectedTraveledTo = new DateTime(1990, 12, 2, 14, 34, 55, DateTimeKind.Local);
+
+            Thread.Sleep(100);
+
+            Clock.Now.Should().BeCloseTo(
+                traveledTo.Add(TimeSpan.FromMilliseconds(100)), DateTimeComparisonPrecision);
+            traveledTo.Should().Be(expectedTraveledTo);
+        }
+
+        [Fact]
+        public void TravelToLocalDate_ShouldTravelToGivenDate_AndReturnLocalDateTimeTraveledTo()
+        {
+            using var tc = new Timecop();
+
+            var traveledTo = tc.TravelTo(1990, 12, 2, DateTimeKind.Local);
+            var expectedTraveledTo = new DateTime(1990, 12, 2, 0, 0, 0, DateTimeKind.Local);
+
+            Thread.Sleep(100);
+
+            Clock.Now.Should().BeCloseTo(
+                traveledTo.Add(TimeSpan.FromMilliseconds(100)), DateTimeComparisonPrecision);
+            traveledTo.Should().Be(expectedTraveledTo);
+        }
+
+        [Fact]
+        public void TravelToRandomFutureLocalTime_ShouldTravel_AndReturnTheLocalDateTimeTraveledTo()
+        {
+            using var tc = new Timecop();
+
+            var traveledTo = tc.TravelTo(o => o.InTheFuture().LocalTime());
+            var currentLocalTime = DateTime.Now;
+
+            Thread.Sleep(100);
+
+            Clock.Now.Should().BeCloseTo(
+                traveledTo.Add(TimeSpan.FromMilliseconds(100)), DateTimeComparisonPrecision);
+            traveledTo.Should().BeAfter(currentLocalTime);
         }
     }
 }
