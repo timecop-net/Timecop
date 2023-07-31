2601d66fc653dc93c424731d904e162b2a1c01e5
Dmytro Khmara <dmytro@dmytrokhmara.com>
2023-07-31 20:55:17 +0100

    feat: add an instance clock.

---
+++ src/Timecop/IClock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TCop
+{
+    public interface IClock
+    {
+        /// <summary>Returns either current or pre-configured local time. Time can be pre-configured by using <see cref="T:TCop.Timecop" />.</summary>
+        DateTime Now { get; }
+
+        /// <summary>Returns either current or pre-configured UTC time. Time can be pre-configured by using <see cref="T:TCop.Timecop" />.</summary>
+        DateTime UtcNow { get; }
+    }
+}
---
+++ test/Timecop.Tests/IClockTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+namespace TCop.Tests
+{
+    public class IClockTests
+    {
+        private static readonly TimeSpan DateTimeComparisonPrecision = TimeSpan.FromMilliseconds(50);
+
+        [Fact]
+        public void UtcNow_TimecopWithTravel_ShouldReturnCurrentTime()
+        {
+            var fiveHours = TimeSpan.FromHours(5);
+
+            using var tc = new Timecop();
+            tc.TravelBy(fiveHours);
+
+            tc.Clock.UtcNow.Should().BeCloseTo(DateTime.UtcNow.Add(fiveHours), DateTimeComparisonPrecision);
+        }
+
+        [Fact]
+        public void Clock_TwoClocksShouldBeTheSameInstance()
+        {
+            using var tc = new Timecop();
+
+            tc.Clock.Should().Be(tc.Clock);
+        }
+    }
+}
